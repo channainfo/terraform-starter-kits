4b408316daf07d1c0e46c8d1721a98e84bdfd570
Channa Ly <channa.info@gmail.com>
2020-12-30 11:16:52 +0700

    Close #17 S3 CORS for direct upload

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,6 +1,24 @@
 resource "aws_s3_bucket" "main" {
   bucket = var.bucket_name
   acl    = "public-read"
+
+
+  # direct upload
+  # https://edgeguides.rubyonrails.org/active_storage_overview.html#example-s3-cors-configuration
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    # ["https://s3-website-test.hashicorp.com"]
+    allowed_origins = var.sites
+    expose_headers = [
+      "Origin",
+      "Content-Type",
+      "Content-MD5",
+      "Content-Disposition"
+    ]
+    max_age_seconds = 3600
+  }
+
 }
 
 resource "aws_s3_bucket_policy" "main" {
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -2,3 +2,8 @@ variable "bucket_name" {
   description = "The name of your bucket ( No dot .)"
   type        = string
 }
+
+variable "sites" {
+  description = "The list of your sites .eg. [https://www.vtenh.com, https://vtenh.com]"
+  type        = list(string)
+}
